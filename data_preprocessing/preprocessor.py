b57b4a828189756857c2207d112fc9db5d93910c
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-01-12 23:04:33 +0100

    add lemmatisation

--- data_preprocessing/preprocessor.py
+++ data_preprocessing/preprocessor.py
@@ -4,16 +4,17 @@ nlp = spacy.load("de_core_news_sm")
 def reprocess(text:str):
     _tokenize(text)
 
-def _tokenize(text:str):
-    doc = nlp(text)
-
-    tokens = []
-    for token in doc:
+def _lemmatize(text:str):
+    lemma = []
+    tokens = _tokenize(text)
+    for token in tokens:
         if _is_full_word_or_punctuation(token):
             continue
-        tokens.append(token.lemma_.lower())
+        lemma.append(token.lemma_.lower())
+    return " ".join(lemma)
 
-    return " ".join(tokens)
+def _tokenize(text:str):
+     return nlp(text)
 
 def _is_full_word_or_punctuation(token)->bool:
     return token.is_stop or token.is_punct or token.is_space
