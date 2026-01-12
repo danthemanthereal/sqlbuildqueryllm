8b6d94622ab4ae359948337ca35883ae6b843e5b
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-01-12 22:55:45 +0100

    tokenize method

--- data_preprocessing/preprocessor.py
+++ data_preprocessing/preprocessor.py
@@ -1,2 +1,23 @@
-def reprocess():
-    pass
+import spacy
+nlp = spacy.load("de_core_news_sm")
+
+def reprocess(text:str):
+    _tokenize(text)
+
+def _tokenize(text:str):
+    doc = nlp(text)
+
+    tokens = []
+    for token in doc:
+        if token.is_stop:
+            continue
+        if token.is_punct:
+            continue
+        if token.is_space:
+            continue
+
+        tokens.append(token.lemma_.lower())
+
+    return " ".join(tokens)
+
+
