c1825df0235ef7495cc142cd9bc24f0a04954f8d
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-01-25 22:38:20 +0100

    start for ner for only tables

---
+++ ner/stat_bot_swiss_table_ner.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pathlib import Path
+
+PROJECT_PATH = Path(__file__).resolve().parents[1]
+
+TABLE_METADATA_CSV_FILE_PATH = PROJECT_PATH.joinpath("data/StatBot_Swiss/meta_data_tables.csv")
+
+table_meta_df = pd.read_csv(TABLE_METADATA_CSV_FILE_PATH)
+print(table_meta_df.columns.tolist())
+only_german_table_df = table_meta_df[table_meta_df.lang == "de"]
+
+#print(only_german_table_df[["name", "discription"]])
+
+
