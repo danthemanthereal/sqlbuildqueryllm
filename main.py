df12764b11e2f57c2b965c5aa13eedead70759fd
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-01-26 22:06:53 +0100

    finish first embedding try

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from data_preprocessing.stat_bot_swiss_preprocessing import table_meta_df
+from data_preprocessing.preprocessor import reprocess
+from data_preprocessing.stat_bot_swiss_preprocessing import table_meta_df, only_german_test_df
 from vector_database.vector_db_for_table_describtion_swit_bot_dataset import collection
 
 only_table_name = list(table_meta_df["name"])
 table_description_df = table_meta_df[["name", "discription"]]
+only_question = list(only_german_test_df["question"])
 
-for table_name in only_table_name:
+for question in only_question:
+    pre_processed_words = reprocess(question)
     results = collection.query(
-        query_texts=table_name,
-        n_results=3
+        query_texts=["".join([w.text for w in pre_processed_words]) + " datenbank tabelle beschreibung"],
+        n_results=1
     )
-    print("Query:", table_name)
-    print("Result:", results['documents'])
-    description = table_description_df.loc[table_description_df["name"] == table_name, "discription"].values[0]
-    print("richtiges ergebnis ",description )
+    top_docs = results["documents"][0]
+    top_scores = results["distances"][0]
+
+    print("Query:", pre_processed_words)
+    for doc, score in zip(top_docs, top_scores):
+        print(score, doc)
+
+
+    #description = table_description_df.loc[table_description_df["name"] == table_name, "discription"].values[0]
+    #print("richtiges ergebnis ",description )
