7082f70acb4492cd8ff80ec6fe863583ce6892d8
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-01-31 15:05:55 +0100

    improve table recognition query search

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from data_preprocessing.preprocessor import reprocess
 from data_preprocessing.stat_bot_swiss_preprocessing import table_meta_df, only_german_test_df
-from vector_database.vector_db_for_table_describtion_swit_bot_dataset import collection
+from vector_database.vector_db_for_table_describtion_swit_bot_dataset import collection, embeddings, model
+from transformers import AutoTokenizer
+from torch import nn, cosine_similarity
+from collections import Counter
 
 only_table_name = list(table_meta_df["name"])
 table_description_df = table_meta_df[["name", "discription"]]
 only_question = list(only_german_test_df["question"])
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-german-cased")
+embedding_dim = 768
+table_embeddings = embeddings
+table_in_query = 0
+
 
 for question in only_question:
-    pre_processed_words = reprocess(question)
-    results = collection.query(
+    #pre_processed_words = reprocess(question)
+    """results = collection.query(
         query_texts=["".join([w.text for w in pre_processed_words]) + " datenbank tabelle beschreibung"],
         n_results=1
     )
@@ -17,7 +25,26 @@ for question in only_question:
 
     print("Query:", pre_processed_words)
     for doc, score in zip(top_docs, top_scores):
-        print(score, doc)
+        print(score, doc)"""
+    #tokens = tokenizer.tokenize(question)
+    """token_counts = Counter(tokens)
+    input_ids = tokenizer(question, return_tensors="pt")["input_ids"]
+    vocab = {token: idx for idx, token in enumerate(token_counts)}
+    vocab_size = tokenizer.vocab_size
+    word_embeddings = nn.Embedding(vocab_size, embedding_dim)(input_ids)
+    bi_lstm = nn.LSTM(input_size=embedding_dim, hidden_size=128, bidirectional=True, batch_first=True)
+    outputs, (hn, cn) = bi_lstm(word_embeddings)
+    query_vector = outputs.mean(dim=1)"""
+    #query_vector = model.encode(tokens, convert_to_tensor=True)
+    #scores = cosine_similarity(query_vector, table_embeddings)
+    #print("scores ")
+    #print(scores)
+    results = collection.query(
+        query_texts=[question],
+        n_results=5
+    )
+    
+
 
 
     #description = table_description_df.loc[table_description_df["name"] == table_name, "discription"].values[0]
--- vector_database/vector_db_for_table_describtion_swit_bot_dataset.py
+++ vector_database/vector_db_for_table_describtion_swit_bot_dataset.py
@@ -1,7 +1,17 @@
+from typing import List
+
+from pandas import DataFrame
 from sentence_transformers import SentenceTransformer
 from data_preprocessing.stat_bot_swiss_preprocessing import table_meta_df
 import chromadb
 client = chromadb.Client()
+
+def _get_embeddings_text(data_frame: DataFrame) -> List[str]:
+    embeddings_texts = []
+    for _, row in data_frame.iterrows():
+        text = f"{row['name']}: {row['discription']}"
+        embeddings_texts.append(text)
+    return embeddings_texts
 
 only_description_df = table_meta_df["discription"].dropna().astype(str).tolist()
 
@@ -9,13 +19,15 @@ table_description_df = table_meta_df[["name", "discription"]]
 
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
-embeddings = model.encode(only_description_df)
+documents_list = table_description_df["discription"].tolist()
+
+embedding_texts = _get_embeddings_text(table_description_df)
+embeddings = model.encode(embedding_texts, convert_to_tensor=True)
 
 collection = client.get_or_create_collection(name="table_description_stat_bot_data_set")
 ids = [f"id{i}" for i in range(len(embeddings))]
-collection.add(embeddings=embeddings,
+collection.add(embeddings=embeddings.tolist(),
                ids=ids,
-               documents=only_description_df,
+               documents=embedding_texts,
                metadatas=[{"name": n} for n in table_description_df["name"]])
-
 
