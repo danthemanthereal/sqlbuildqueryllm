b2f6b5822be4f890d45b3b9b9d05e37fd8adaf57
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-02-04 17:45:40 +0100

    comment current main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer
 from torch import nn, cosine_similarity
 from collections import Counter
 
-only_table_name = list(table_meta_df["name"])
+"""only_table_name = list(table_meta_df["name"])
 table_description_df = table_meta_df[["name", "discription"]]
 only_question = list(only_german_test_df["question"])
 query_end_question = query_question_test_df
@@ -18,7 +18,7 @@ table_not_in_query = 0
 
 for question in only_question:
     #pre_processed_words = reprocess(question)
-    """results = collection.query(
+    results = collection.query(
         query_texts=["".join([w.text for w in pre_processed_words]) + " datenbank tabelle beschreibung"],
         n_results=1
     )
@@ -27,16 +27,16 @@ for question in only_question:
 
     print("Query:", pre_processed_words)
     for doc, score in zip(top_docs, top_scores):
-        print(score, doc)"""
+        print(score, doc)
     #tokens = tokenizer.tokenize(question)
-    """token_counts = Counter(tokens)
+    token_counts = Counter(tokens)
     input_ids = tokenizer(question, return_tensors="pt")["input_ids"]
     vocab = {token: idx for idx, token in enumerate(token_counts)}
     vocab_size = tokenizer.vocab_size
     word_embeddings = nn.Embedding(vocab_size, embedding_dim)(input_ids)
     bi_lstm = nn.LSTM(input_size=embedding_dim, hidden_size=128, bidirectional=True, batch_first=True)
     outputs, (hn, cn) = bi_lstm(word_embeddings)
-    query_vector = outputs.mean(dim=1)"""
+    query_vector = outputs.mean(dim=1)
     #query_vector = model.encode(tokens, convert_to_tensor=True)
     #scores = cosine_similarity(query_vector, table_embeddings)
     #print("scores ")
@@ -60,4 +60,4 @@ print("table not in query:", table_not_in_query)
 
 
     #description = table_description_df.loc[table_description_df["name"] == table_name, "discription"].values[0]
-    #print("richtiges ergebnis ",description )
+    #print("richtiges ergebnis ",description )"""
