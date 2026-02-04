f9b5404b8f08d5a2b4e171171b92e1238fa06ef5
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-02-04 21:54:21 +0100

    add preprocessor german spider

--- schema_linking/cross_encoder_approach.py
+++ schema_linking/cross_encoder_approach.py
@@ -11,13 +11,14 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 def get_similarity_tables_and_sentence(sentence_tokens: list[str], question: str):
     tables = get_all_tables(G)
     token_embeddings = model.encode(sentence_tokens)
-    # for tables of german spider table_embeddings = model.encode(tables)
+    # for tables of german spider
+   # table_embeddings = model.encode(tables)
     # for table embdegging swiss tat bot
     descriptions = table_meta_df["discription"].tolist()
 
     # Jeden String durch reprocess schicken
     #processed_descriptions = [reprocess(desc) for desc in descriptions]
-    table_embeddings = model.encode(descriptions)
+    table_embeddings = model.encode(tables)
     threshold = 0.6
 
 
@@ -25,19 +26,20 @@ def get_similarity_tables_and_sentence(sentence_tokens: list[str], question: str
         for j, table_emb in enumerate(table_embeddings):
             similarity = util.cos_sim(token_emb, table_emb).item()
             if similarity >= threshold:
-                matching_row = table_meta_df[table_meta_df["discription"] == descriptions[j]]
-                table_value = ""
-                if not matching_row.empty:
-                    table_value = matching_row.iloc[0]["name"]  # Name der ersten passenden Zeile
-                    print(f"Name der Zeile mit Description '{descriptions[j]}': {table_value}")
+                #matching_row = table_meta_df[table_meta_df["discription"] == descriptions[j]]
+                #table_value = ""
+                #if not matching_row.empty:
+                 #   table_value = matching_row.iloc[0]["name"]  # Name der ersten passenden Zeile
+                  #  print(f"Name der Zeile mit Description '{descriptions[j]}': {table_value}")
                 print("Frage:", question)
                 print(f"Wort: {sentence_tokens[i]}")
-                print("Table ", table_value)
-                matchingquery = query_question_test_df[query_question_test_df["question"] == question]
-                query_value = ""
-                if not matchingquery.empty:
+                print(f"Table {tables[j]}")
+               # print("Table ", table_value)
+               # matchingquery = query_question_test_df[query_question_test_df["question"] == question]
+               # query_value = ""
+               # if not matchingquery.empty:
                     # Direkt den Wert der 'query'-Spalte holen
-                    query_value = matchingquery.iloc[0]["query"]
-                print("Query ", query_value)
-                print(f"is in query {table_value in query_value}")
+                #    query_value = matchingquery.iloc[0]["query"]
+               # print("Query ", query_value)
+               # print(f"is in query {table_value in query_value}")
                 print(f"Ähnlichkeit: {similarity:.3f}\n")
