25e1cea47fa8857f8511098cf91c616a35002241
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-02-24 18:02:28 +0100

    add self correction and will do chess with spacy

---
+++ schema_linking/custom_chess_agentframe_work/information_retriever_agent/extract_key_words.py
@@ -0,0 +1,13 @@
+# Schritt 1 : sucht Keywords (Tabellen, Spalten, ... ) aus Frage
+def return_extracted_key_words(question: str) -> list:
+
+    """
+    Funktioiert so man gibt Frage und Promot
+    bekommt Liste von Key wortts potenzielle Tablellen , Spalten, Bedingungen ? ....
+    :param question:
+    :return:
+    """
+    keywords = []
+    return keywords
+
+# eigentlich llm  hier testweise einfach mit spacy so stop wörter usw entfernen
---
+++ schema_linking/custom_chess_agentframe_work/information_retriever_agent/retrieve_entities.py
@@ -0,0 +1,6 @@
+# Schritt 2
+# Dieser Code definiert eine Klasse RetrieveEntity,
+# die aus einer Frage und einem Hinweis (Hint)
+# automatisch relevante Spalten und Werte einer Datenbank identifiziert.
+
+
