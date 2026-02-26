5428ebdd4e77dda482721b095978661da0c2a93e
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-02-26 23:34:36 +0100

    with table anchor mebedding

--- schema_linking/use_table_name_as_anchor.py
+++ schema_linking/use_table_name_as_anchor.py
@@ -4,6 +4,8 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from adjustText import adjust_text
 from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 def get_most_similar_table_with_anchor(word: str):
     tables = get_all_tables()
@@ -11,7 +13,7 @@ def get_most_similar_table_with_anchor(word: str):
     multi_lang_version = "intfloat/multilingual-e5-small"
     model = SentenceTransformer(multi_lang_version)
 
-    embeddings = model.encode(tables)
+    table_embeddings = model.encode(tables)
 
     """pca = PCA(n_components=2)
     reduced = pca.fit_transform(embeddings)
@@ -48,3 +50,29 @@ def get_most_similar_table_with_anchor(word: str):
     plt.ylabel("PC2")
 
     plt.show()"""
+
+    word_embeddings = model.encode([word])
+
+
+    # Calculate similarity features
+    similarity_features = cosine_similarity(word_embeddings, table_embeddings)
+
+    similarity_features = cosine_similarity(
+        word_embeddings,
+        table_embeddings
+    )
+
+    # Array flach machen
+    similarities = similarity_features[0]
+
+    # Top 5 Indizes finden (höchste Ähnlichkeit)
+    top_k = 5
+    top_indices = np.argsort(similarities)[-top_k:][::-1]
+
+    #print("Top 5 ähnlichste Ergebnisse:\n")
+    predicted_tables = []
+    for rank, idx in enumerate(top_indices, 1):
+       # print(f"{rank}. Tabelle: {tables[idx]}")
+        #print(f"   Similarity: {similarities[idx]:.4f}\n")
+        predicted_tables.append(tables[idx])
+    return predicted_tables
