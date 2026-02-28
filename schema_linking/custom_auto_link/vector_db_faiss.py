3f825d769246483d4d091f265cd6319aa8d38ca6
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-02-28 11:56:28 +0100

    add the last tables with descriptions

--- schema_linking/custom_auto_link/vector_db_faiss.py
+++ schema_linking/custom_auto_link/vector_db_faiss.py
@@ -15,97 +15,32 @@ def embed_documents(batch_size: int = 32):
 
     log = {}
     db_dict_path = "/Users/danielschmidt/Desktop/sqlbuildqueryllm/data/dataset_spider_de/spider/database"
-    json_file = "/Users/danielschmidt/Desktop/sqlbuildqueryllm/data/dataset_spider_de/multispider/with_original_value/tables_de.json"
-    with open(json_file, "r", encoding="utf-8") as f:
-        db_schemas = json.load(f)
-
-    for db in db_schemas:
-        db_id = db["db_id"]
-        tables = db["table_names"]
-        columns = db["column_names"]
-        column_types = db["column_types"]
-        original_tables = db["table_names_original"]
-        original_columns = db["column_names_original"]
-        all_descriptions = []
-        metadata_mapping = []
-        idx_col = 0
+    json_file = "/Users/danielschmidt/Desktop/sqlbuildqueryllm/data/dataset_spider_de/multispider/with_original_value/table_map_with_desc.json"
+    with open(json_file, 'r', encoding='utf-8') as f:
+        all_table_map = json.load(f)
+    for table in all_table_map:
+        table_name = table["table_name"]
+        table_description = table["table_description"]
+        columns = table["column_names"]
+        columns_description = table["column_descriptions"]
+        db_id = table["db_id"]
         embedd_fais_dict_path = f"/Users/danielschmidt/Desktop/sqlbuildqueryllm/schema_linking/custom_auto_link/embedded_documents/{db_id}"
-
-        for ind, (table_ind, col_name) in enumerate(columns):
-            if col_name == "*":
-                continue
+        descriptions_to_embed = []
+        metadata_per_col = []
+        for idx, col in enumerate(columns):
             col_info = dict()
-            col_info["column_name"] = col_name
-            table_name = tables[table_ind]
-            english_col_tupel = original_columns[ind]
-            english_table_id = english_col_tupel[0]
-            english_table = original_tables[english_table_id]
-            english_col_name = english_col_tupel[1]
-            column_data_type = column_types[idx_col]
-            column_values = []
-            no_file_exists = [
-                "bike_1",
-                "wta_1",
-                "formula_1",
-                "college_2",
-                "sakila_1",
-                "flight_4",
-                "soccer_1",
-                "baseball_1",
-                "store_1"
-            ]
-            if db_id not in no_file_exists:
-                pass
-              ##  column_values = get_column_distinct_column_values(english_table,
-                #                                                  english_col_name,
-                 #                                                 db_id)
+            col_info["column_name"] = col
+            current_description = columns_description[idx]
+            descriptions_to_embed.append(
+                {
+                    "Tabelle: ": table_name,
+                    "Beschreibung von Tabelle: ": table_description,
+                    "Spalte: ": col,
+                    "Beschreibung der Spalte: ": current_description
+                }
+            )
 
-            description = (f"Tabelle: {table_name}\n"
-                           f"Spalte: {col_name}\n"
-                           f"Datentyp: {column_data_type}\n")
-                          # f"Werte: {column_values}")
-            all_descriptions.append(description)
-            distinct_values_of_current_col = []
-            invalid_dbs = [
-                "wta_1",
-                "formula_1",
-                "college_2",
-                "sakila_1",
-                "flight_4",
-                "soccer_1",
-                "baseball_1",
-                "store_1"
-            ]
-            if db_id not in invalid_dbs:
-                pass
-                # distinct_values_of_current_col = get_column_distinct_column_values(table_name, col_name, db_id)
-            metadata_mapping.append({
-                "table": table_name,
-                "column": col_name,
-                "column_type": column_data_type,
-              #  "column_value": column_values,
 
-            })
-            idx_col += 1
-
-        db_embeddings = []
-        for i in range(0, len(all_descriptions), batch_size):
-            batch_descriptions = all_descriptions[i:i + batch_size]
-            batch_embeddings = model.encode(
-                batch_descriptions,
-                convert_to_numpy=True,
-                batch_size=batch_size,
-                device="cpu", )
-            db_embeddings.extend(batch_embeddings)
-
-        dimension = len(db_embeddings[0])
-        index = faiss.IndexFlatL2(dimension)
-        index.add(np.array(db_embeddings, dtype=np.float32))
-        os.makedirs(embedd_fais_dict_path, exist_ok=True)
-        faiss.write_index(index, os.path.join(embedd_fais_dict_path, "index.faiss"))
-
-        with open(os.path.join(embedd_fais_dict_path, "metadata.json"), "w", encoding="utf-8") as f_meta:
-            json.dump(metadata_mapping, f_meta, ensure_ascii=False, indent=2)
 
 
 def get_column_distinct_column_values(table_name: str, column_name: str, db_path: str) -> list:
@@ -134,3 +69,96 @@ def get_column_distinct_column_values(table_name: str, column_name: str, db_path
     finally:
         conn.close()
     return picklist
+
+
+"""with open(json_file, "r", encoding="utf-8") as f:
+    db_schemas = json.load(f)
+
+for db in db_schemas:
+    db_id = db["db_id"]
+    tables = db["table_names"]
+    columns = db["column_names"]
+    column_types = db["column_types"]
+    original_tables = db["table_names_original"]
+    original_columns = db["column_names_original"]
+    all_descriptions = []
+    metadata_mapping = []
+    idx_col = 0
+    embedd_fais_dict_path = f"/Users/danielschmidt/Desktop/sqlbuildqueryllm/schema_linking/custom_auto_link/embedded_documents/{db_id}"
+
+    for ind, (table_ind, col_name) in enumerate(columns):
+        if col_name == "*":
+            continue
+        col_info = dict()
+        col_info["column_name"] = col_name
+        table_name = tables[table_ind]
+        english_col_tupel = original_columns[ind]
+        english_table_id = english_col_tupel[0]
+        english_table = original_tables[english_table_id]
+        english_col_name = english_col_tupel[1]
+        column_data_type = column_types[idx_col]
+        column_values = []
+        no_file_exists = [
+            "bike_1",
+            "wta_1",
+            "formula_1",
+            "college_2",
+            "sakila_1",
+            "flight_4",
+            "soccer_1",
+            "baseball_1",
+            "store_1"
+        ]
+        if db_id not in no_file_exists:
+            pass
+          ##  column_values = get_column_distinct_column_values(english_table,
+            #                                                  english_col_name,
+             #                                                 db_id)
+
+        description = (f"Tabelle: {table_name}\n"
+                       f"Spalte: {col_name}\n"
+                       f"Datentyp: {column_data_type}\n")
+                      # f"Werte: {column_values}")
+        all_descriptions.append(description)
+        distinct_values_of_current_col = []
+        invalid_dbs = [
+            "wta_1",
+            "formula_1",
+            "college_2",
+            "sakila_1",
+            "flight_4",
+            "soccer_1",
+            "baseball_1",
+            "store_1"
+        ]
+        if db_id not in invalid_dbs:
+            pass
+            # distinct_values_of_current_col = get_column_distinct_column_values(table_name, col_name, db_id)
+        metadata_mapping.append({
+            "table": table_name,
+            "column": col_name,
+            "column_type": column_data_type,
+          #  "column_value": column_values,
+
+        })
+        idx_col += 1
+
+    db_embeddings = []
+    for i in range(0, len(all_descriptions), batch_size):
+        batch_descriptions = all_descriptions[i:i + batch_size]
+        batch_embeddings = model.encode(
+            batch_descriptions,
+            convert_to_numpy=True,
+            batch_size=batch_size,
+            device="cpu", )
+        db_embeddings.extend(batch_embeddings)
+
+    dimension = len(db_embeddings[0])
+    index = faiss.IndexFlatL2(dimension)
+    index.add(np.array(db_embeddings, dtype=np.float32))
+    os.makedirs(embedd_fais_dict_path, exist_ok=True)
+    faiss.write_index(index, os.path.join(embedd_fais_dict_path, "index.faiss"))
+
+    with open(os.path.join(embedd_fais_dict_path, "metadata.json"), "w", encoding="utf-8") as f_meta:
+        json.dump(metadata_mapping, f_meta, ensure_ascii=False, indent=2)"""
+
